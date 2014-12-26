c04a2062b62c704841929ffdbdb69913db039286
ptrckekn <dmitrivada@gmail.com>
2014-12-26 12:57:21 -0500

    Created Decrypt Method, Updated Main
    Created a method to decrypt an encrypted message. Updated main to give
    the user the option to decrypt or encrypt. Wrote in controls to handle
    invalid user entries.

--- EncryptionDriver.java
+++ EncryptionDriver.java
@@ -5,20 +5,40 @@ public class EncryptionDriver
 {
 	public static void main(String args[])
 	{
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a decryption passkey.");
-		String userPassKey = input.nextLine();
-		System.out.println("You entered: " + "\"" + userPassKey + "\"");
-		System.out.println();
-		System.out.println("Enter a message to be encrypted.");
-		String userMessage = input.nextLine();
-		System.out.println("You entered: " + "\"" + userMessage + "\"");
-		System.out.println("All spaces and special characters will now be removed.");
-		System.out.println("Only the letters will be encrypted and read back to you.");
-		System.out.println();
-		System.out.println("Your message after encryption is: ");
-		
 		EncryptionMethod myEncrypt = new EncryptionMethod();
-		myEncrypt.encryptionTablet(userPassKey, userMessage);
-	}
+		Scanner input = new Scanner(System.in);
+		//Scanner inputTwo = new Scanner(System.in);
+		String answer = "0";
+		
+		while(!answer.equals("1") && !answer.equals("2"))//to control invalid entries
+		{
+			System.out.println("If you would like to ENCRYPT a message press (1) then enter.");
+			System.out.println("If you would like to DECRYPT a message press (2) then enter");
+			answer = input.nextLine();
+			if(answer.equals("1"))
+			{
+				System.out.println("Enter a unique passkey.");
+				String userPassKey = input.nextLine();
+				System.out.println("Enter a message to be encrypted.");
+				String userMessage = input.nextLine();
+				System.out.println("Your message after encryption is: ");
+			
+				myEncrypt.encrypt(userPassKey, userMessage);//method encrypts the users message
+			}
+			else if(answer.equals("2"))
+			{
+				System.out.println("Enter the correct passkey.");
+				String decryptionPassKey = input.nextLine();
+				System.out.println("Enter the encrypted message.");
+				String encryptedMessage = input.nextLine();
+				System.out.println("You1r message after decryption is: ");
+			
+				myEncrypt.decrypt(decryptionPassKey, encryptedMessage);//decrypts the encrypted message
+			}
+			else
+			{
+				System.out.println("You must enter either (1) or (2) please try again.");//if invalid entry is entered
+			}
+		}	
+	}		
 }
--- EncryptionMethod.java
+++ EncryptionMethod.java
@@ -1,7 +1,8 @@
 package vigenereCipher;
+import java.util.Arrays;
 public class EncryptionMethod
 {
-	public void encryptionTablet(String key, String message) 
+	void encrypt(String key, String message) 
 	{
 		int ROWS = 26;
 		int COLS = 26;
@@ -42,12 +43,11 @@ public class EncryptionMethod
 		}//end for loop
 		
 		
-		for(int i = 0; i < Math.max(keyArray.length, messageArray.length); i++)
+		for(int i = 0; i < messageArray.length; i++)
 		{	
 			//assign each keyChar char to variable and increment
 			char keyChar = keyArray[i%keyArray.length];
 		
-			
 			//assign each messageChar char to variable and increment
 			char messageChar = messageArray[i%messageArray.length];
 		
@@ -58,5 +58,66 @@ public class EncryptionMethod
 			int messageCharPosition = (int)messageChar;
 			System.out.print(tablet[keyCharPosition-65][messageCharPosition-65]);
 		}//end for loop
-	}//end encryptionTablet
+	}//end encrypt method
+	
+	void decrypt(String key, String encryptedMessage)
+	{
+		int ROWS = 26;
+		int COLS = 26;
+		String keyUpcaseNoSpace = key.toUpperCase().replaceAll("[^a-zA-Z]+", "");
+		String encryptedMessageUpcaseNoSpace = encryptedMessage.toUpperCase().replaceAll("[^a-zA-Z]+", "");
+		
+		// 2d alphabet char array as a tablet to encrypt message
+		char[][] tablet = new char[ROWS][COLS];
+		
+		// passkey String turned into char array
+		char[] keyArray = keyUpcaseNoSpace.toCharArray();
+		
+		// message String turned into char array
+		char[] encryptedMessageArray = encryptedMessageUpcaseNoSpace.toCharArray();
+		
+		// for loops to generate the tablet
+		
+		for(int i = 0; i < 26; i++)
+		{
+			for(int j = 0; j < 26; j++)
+			{
+				// creates an int that will be cast into a char
+				int letter = i + j;
+				
+				// stops int from going above 26
+				if (letter >= 26)
+				{
+					letter = letter - 26;
+				}
+				// Upper case alphabet in char list starts at 65 mindprod.com/jgloss/ascii.html#ASCIITABLE
+				letter = letter + 65;
+				
+				// cast int to char
+				char gridLetter = (char)letter;
+				
+				tablet[i][j] = gridLetter;;
+			}//end for loop
+		}//end for loop
+		
+		for(int i = 0; i < encryptedMessageArray.length; i++)
+		{	
+			//assign each keyChar char to variable and increment
+			char keyChar = keyArray[i%keyArray.length];
+			
+			//assign each messageChar char to variable and increment
+			char encryptedMessageChar = encryptedMessageArray[i%encryptedMessageArray.length];
+			
+			//cast char to int
+			int keyCharPosition = (int)keyChar;
+					
+			for( int j = 0; j < tablet.length; j++)
+			{
+				if(Arrays.asList(tablet[j][keyCharPosition-65]).contains(encryptedMessageChar))
+				{
+					System.out.print(tablet[j][0]);
+				}
+			}//end for loop
+		}//end for loop
+	}//end decrypt method
 }//end class
